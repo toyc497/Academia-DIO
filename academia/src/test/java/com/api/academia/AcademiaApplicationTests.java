b1df79d1a6527ea1e926cf131808d16eb419eeb4
Matheus Fillipe <67832413+toyc497@users.noreply.github.com>
2023-01-26 13:37:47 -0300

    Base da API

---
+++ academia/src/test/java/com/api/academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
